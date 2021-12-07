2c68256bce7e09418e82fef5a3fb66246f2cc689
LEKMAD <mohamed.lekamd@um5s.net.ma>
2021-12-07 19:09:21 +0100

    fixed image controlle

--- app/controllers/photo.controller.js
+++ app/controllers/photo.controller.js
@@ -1,133 +1,133 @@
-const db = require("../models");
-const Image = db.Images;
-const fs = require('fs');
-const path = require("path");
+const Image = require("../models/image.model");
 
-
+const saveImage = async (title, image) => {
+  await Image.create({
+    title,
+    image,
+  });
+};
 // Create and Save a new Image
-exports.create = (req, res) => {
-  const obj = JSON.parse(JSON.stringify(req.body))
-  console.log('BODY: ', obj)
-    if (!req.body.title) {
-        res.status(400).send({ message: "Content can not be empty!" });
-        return;
-      }
-    
-      // Create a Image
-      const image = new Image({
-        title: req.body.title,
-        img: {
-          data: fs.readFileSync(path.join(__dirname + '/public/images/' + req.files.filename.name)),
-          contentType: 'image/png'
-      }
+exports.create = async (req, res) => {
+  if (!req.files) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+  if (!Array.isArray(req.files.filename)) {
+    try {
+      const {name,data} = req.files.filename;
+      const image = await saveImage(name, data);
+      res.status(200).send(image);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  } else {
+    try {
+      req.files.filename.forEach(async (file) => {
+        const image = saveImage(file.name, file.data)
       });
-    
-      // Save Image in the database
-      Image
-        .save(image)
-        .then(data => {
-          res.send(data);
-        })
-        .catch(err => {
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while creating the Image."
-          });
-        });
+      res.status(200).send("images were created successfully");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  }
 };
 
 // Retrieve all Images from the database.
 exports.findAll = (req, res) => {
-    Image.findAll()
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving images."
-        });
-      });
-};
-
-// Find a single Image with an id
-exports.findOne = (req, res) => {
-    const id = req.params.id;
-
-    Image.findById(id)
-      .then(data => {
-        if (!data)
-          res.status(404).send({ message: "Not found Image with id " + id });
-        else res.send(data);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .send({ message: "Error retrieving Image with id=" + id });
-      });
-};
-
-// Update a Image by the id in the request
-exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-          message: "Data to update can not be empty!"
-        });
-      }
-    
-      const id = req.params.id;
-    
-      Image.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-        .then(data => {
-          if (!data) {
-            res.status(404).send({
-              message: `Cannot update Image with id=${id}. Maybe Image was not found!`
-            });
-          } else res.send({ message: "Image was updated successfully." });
-        })
-        .catch(err => {
-          res.status(500).send({
-            message: "Error updating Image with id=" + id
-          });
-        });
-};
-
-// Delete a Image with the specified id in the request
-exports.delete = (req, res) => {
-    const id = req.params.id;
-
-    Image.findByIdAndRemove(id)
-      .then(data => {
-        if (!data) {
-          res.status(404).send({
-            message: `Cannot delete Image with id=${id}. Maybe Image was not found!`
-          });
-        } else {
-          res.send({
-            message: "Image was deleted successfully!"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete Image with id=" + id
-        });
-      });
-};
-
-// Delete all Images from the database.
-exports.deleteAll = (req, res) => {
-    Image.deleteMany({})
-    .then(data => {
-      res.send({
-        message: `${data.deletedCount} Images were deleted successfully!`
-      });
+  Image.find()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all Images."
+        message: err.message || "Some error occurred while retrieving images.",
       });
     });
 };
 
+// Find a single Image with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Image.findById(id)
+    .then((data) => {
+      if (!data)
+        res.status(404).send({ message: "Not found Image with id " + id });
+      else res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error retrieving Image with id=" + id });
+    });
+};
+
+// Update a Image by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+  console.log("req: ", req.files);
+  const id = req.params.id;
+  const newImage = {
+    title: req.files.filename.name,
+    img: {
+      data: req.files.data,
+      contentType: "image/png",
+    },
+  };
+  Image.findByIdAndUpdate(id, newImage, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Image with id=${id}. Maybe Image was not found!`,
+        });
+      } else res.send({ message: "Image was updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Image with id=" + id,
+      });
+    });
+};
+
+// Delete a Image with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Image.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Image with id=${id}. Maybe Image was not found!`,
+        });
+      } else {
+        res.send({
+          message: "Image was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Image with id=" + id,
+      });
+    });
+};
+
+// Delete all Images from the database.
+exports.deleteAll = (req, res) => {
+  Image.deleteMany({})
+    .then((data) => {
+      res.send({
+        message: `${data.deletedCount} Images were deleted successfully!`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Images.",
+      });
+    });
+};
--- app/models/image.model.js
+++ app/models/image.model.js
@@ -1,22 +1,22 @@
-module.exports = mongoose => {
-    var schema = mongoose.Schema(
-      {
-        title: String,
-        img:
-        {
-            data: Buffer,
-            contentType: String
-        }
-      },
-      { timestamps: true }
-    );
-  
-    schema.method("toJSON", function() {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
-    });
-  
-    const Image = mongoose.model("image", schema);
-    return Image;
-  };
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ImageSchema = new Schema(
+  {
+    title: String,
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+ImageSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+const ImageModel = mongoose.model("image", ImageSchema);
+module.exports = ImageModel;
